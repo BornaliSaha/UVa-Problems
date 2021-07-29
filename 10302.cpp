5f33c40eb4b9467396da9968f07573dd1cc317d3
Bornali Saha <68811059+BornaliSaha@users.noreply.github.com>
2021-07-29 19:53:08 +0600

    Summation of Polynomials

---
+++ 10302.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long int x;
+    while(cin>>x)
+    {
+        long long res=0;
+        res= (x*x*(x+1)*(x+1))/4;
+        cout<<res<<endl;
+
+    }
+    return 0;
+}
+
