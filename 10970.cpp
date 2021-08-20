e482c20c1bedc9b005148e5866e512dd1dd7b963
Bornali Saha <68811059+BornaliSaha@users.noreply.github.com>
2021-08-20 09:33:42 +0600

    Big Chocolate

---
+++ 10970.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,m,ans;
+
+    while(cin>>m>>n)
+    {
+        ans=0;
+        ans=(m*n)-1;
+        cout<<ans<<endl;
+    }
+
+    return 0;
+}
